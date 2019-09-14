2dced1355a2a24ffafc5428419110f5ad1a905a1
Adrien Chinour <adrien.chinour@protonmail.com>
2019-09-14 18:25:26 +0200

    clean code

--- app/src/main/java/com/example/message/MainActivity.java
+++ app/src/main/java/com/example/message/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.message;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -15,27 +14,44 @@ import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Use to validating SEND_SMS permission
+     */
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
 
+    /**
+     * Save input message to send
+     */
     private String message;
 
+
+    /**
+     * Save all input phone numbers
+     */
     private String[] phoneNumbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED);
     }
 
+    /**
+     * Handle form when user want to send SMS on clicking submit button
+     */
     public void handleForm(View view) {
         // get message and phone number from SMS form
         EditText phoneInput = findViewById(R.id.phone);
         EditText messageInput = findViewById(R.id.message);
 
         //convert input to string
-        this.phoneNumbers = phoneInput.getText().toString().split(";");
+        this.phoneNumbers = phoneInput.getText().toString().replaceAll("\\s", "").split(";");
         this.message = messageInput.getText().toString();
+
+        // validating message length (should be > 0)
+        if (message.length() < 1) {
+            Toast.makeText(getApplicationContext(), "Veuillez écrire un message", Toast.LENGTH_LONG).show();
+        }
 
         // check permissions to send SMS
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
@@ -45,26 +61,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * send SMS for each phoneNumbers provided by user
+     */
     private void sendSMS() {
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_LONG;
 
         for (String phoneNumber : this.phoneNumbers) {
+
+            // validating phoneNumber before send message
             if (!phoneNumber.matches("^[+]?[0-9]{4,13}$")) {
-                Toast toast = Toast.makeText(context, "Le numéro de téléphone " + phoneNumber + " n'est pas valide.", duration);
-                toast.show();
+                Toast.makeText(getApplicationContext(), "Le numéro de téléphone " + phoneNumber + " n'est pas valide.", Toast.LENGTH_LONG).show();
                 return;
             }
             SmsManager.getDefault().sendTextMessage(phoneNumber, null, this.message, null, null);
         }
 
         // send validation message to user
-        Toast toast = Toast.makeText(context, "Votre message a bien été envoyé.", duration);
-        toast.show();
+        Toast.makeText(getApplicationContext(), "Votre message a bien été envoyé.", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        // MY_PERMISSIONS_REQUEST_SEND_SMS : send SMS if user is ok for SEND_SMS permission
         if (requestCode == MY_PERMISSIONS_REQUEST_SEND_SMS) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 sendSMS();
